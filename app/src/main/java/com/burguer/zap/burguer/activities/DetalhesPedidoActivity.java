76c33a0d7793ef8e73aa47b622359b8fa24713e9
Kevin Aguiar <kevinfaveri@hotmail.com>
2017-11-19 12:48:28 -0200

    ADD: Activities para lista e detalhe de pedidos

---
+++ app/src/main/java/com/burguer/zap/burguer/activities/DetalhesPedidoActivity.java
@@ -0,0 +1,14 @@
+package com.burguer.zap.burguer.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.burguer.zap.burguer.R;
+
+public class DetalhesPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_pedido);
+    }
+}
---
+++ app/src/main/java/com/burguer/zap/burguer/activities/ListaPedidosActivity.java
@@ -0,0 +1,15 @@
+package com.burguer.zap.burguer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.burguer.zap.burguer.R;
+
+public class ListaPedidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pedidos);
+    }
+}
